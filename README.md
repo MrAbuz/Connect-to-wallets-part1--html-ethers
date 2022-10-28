69eaa81a1bc57dd6756284607dee43c1f73f4dbe
MrAbuz <miguelmrosario@gmail.com>
2022-10-28 23:07:50 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+1. First we're gonna do everything in HTML / JS
+2. NextJS / React and "raw" ethers
+   Then we're gonna get more advanced, and use some more advanced tooling for the web3 space (NextJS and a package):
+3. NextJS & "web3-react"
+4. NextJS & "react-moralis"
+5. NextJS & "web3Modal"
+6. NextJS & "useDapp"
---
+++ index.js
@@ -0,0 +1,127 @@
+const { ethers } = require("ethers");
+//instead of altering this to "import" to the frontend js version, we installed the package "browserify" (excel), which created the folder "dist" with "bundle.js" that is
+//the modified version of "index.js" to the frontend js.
+//we adapted the html to respond to "bundle.js" instead of "index.js" (I dont like this option as much, let's see if we'll keep using it)
+
+async function connect() {
+  if (typeof window.ethereum !== "undefined") {
+    await ethereum.request({ method: "eth_requestAccounts" });
+  }
+}
+
+async function execute() {
+  // address
+  // contract ABI (blueprint to interact with a contract)
+  // function
+  // node connection
+  const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+  const abi = [
+    {
+      stateMutability: "payable",
+      type: "fallback",
+    },
+    {
+      inputs: [
+        {
+          internalType: "string",
+          name: "_name",
+          type: "string",
+        },
+        {
+          internalType: "uint256",
+          name: "_favoriteNumber",
+          type: "uint256",
+        },
+      ],
+      name: "addPerson",
+      outputs: [],
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+    {
+      inputs: [
+        {
+          internalType: "string",
+          name: "",
+          type: "string",
+        },
+      ],
+      name: "nameToFavoriteNumber",
+      outputs: [
+        {
+          internalType: "uint256",
+          name: "",
+          type: "uint256",
+        },
+      ],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [
+        {
+          internalType: "uint256",
+          name: "",
+          type: "uint256",
+        },
+      ],
+      name: "people",
+      outputs: [
+        {
+          internalType: "uint256",
+          name: "favoriteNumber",
+          type: "uint256",
+        },
+        {
+          internalType: "string",
+          name: "name",
+          type: "string",
+        },
+      ],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [],
+      name: "retrieve",
+      outputs: [
+        {
+          internalType: "uint256",
+          name: "",
+          type: "uint256",
+        },
+      ],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [
+        {
+          internalType: "uint256",
+          name: "_favoriteNumber",
+          type: "uint256",
+        },
+      ],
+      name: "store",
+      outputs: [],
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+    {
+      stateMutability: "payable",
+      type: "receive",
+    },
+  ];
+
+  const provider = new ethers.providers.Web3Provider(window.ethereum);
+  const signer = provider.getSigner(); //this is going to get the connected account
+  const contract = new ethers.Contract(contractAddress, abi, signer);
+  await contract.store("42");
+}
+
+module.exports = {
+  connect,
+  execute,
+};
+
+//19:05
